5255b3cf9476ad88404a42052175cb0bd80318dc
Program Tech <techprogram9090@gmail.com>
2026-06-27 16:40:18 +0500

    Add files via upload

---
+++ day9-typecasting.py
@@ -0,0 +1,30 @@
+   #Question 1
+# Python
+# x = "10.5"
+# y = "20"
+# Change x into a float and y into an integer, then plus (+)
+# them together. Write the code and the final answer.
+
+x = "10.5"
+y = "20"
+
+print(float(x) + int(y))
+
+# Question 2
+# Python
+# p = 15
+# q = "5"
+# print(str(p) + q)
+# What will be the output of this one? 
+# (Hint: Think about what happens when you plus + two strings together).
+
+p = 15 
+q = "5"
+
+print(str(p) +q )
+
+# Python
+# age = input("Enter your age: ")
+# Give it a try!
+
+age = input("enter your age")
